92050dbff4db86df9d99d6476c5a7f2d9f2dee3e
Karamansky <karamansky@gmail.com>
2024-02-06 22:39:25 +0200

    Added fix for 404

--- wp-content/themes/topiar/404.php
+++ wp-content/themes/topiar/404.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
-<section class="error404">
+<section class="error-404">
 	<div class="wrapper">
-		<div class="error404__inner">
-			<div class="error404__header">
-				<h3 class="error404__title"><?php esc_html_e( 'На жаль, за вашим запитом нічого не знайдено, але ...', 'tp' ); ?></h3>
-				<div class="error404__number">
+		<div class="error-404__inner">
+			<div class="error-404__header">
+				<h3 class="error-404__title"><?php esc_html_e( 'На жаль, за вашим запитом нічого не знайдено, але ...', 'tp' ); ?></h3>
+				<div class="error-404__number">
 					404
 				</div>
-				<h3 class="error404__title"><?php esc_html_e( '... Ви можете подивитись щось інше ', 'tp' ); ?></h3>
+				<h3 class="error-404__title"><?php esc_html_e( '... Ви можете подивитись щось інше ', 'tp' ); ?></h3>
 			</div>
-			<div class="error404__buttons">
-				<a href="/" class="btn btn--orange error404__button"><?php _e('Каталог', 'tp'); ?></a>
-				<a href="/kontakty" class="btn btn--green error404__button"><?php _e('Звертайтесь', 'tp'); ?></a>
+			<div class="error-404__buttons">
+				<a href="/" class="btn btn--orange error-404__button"><?php _e('Каталог', 'tp'); ?></a>
+				<a href="/kontakty" class="btn btn--green error-404__button"><?php _e('Звертайтесь', 'tp'); ?></a>
 			</div>
 		</div>
 	</div>
