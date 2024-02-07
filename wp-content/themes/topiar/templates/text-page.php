e9ffd89f0452519015933dfcf71e1272eb88ed7e
Karamansky <karamansky@gmail.com>
2024-02-07 16:04:22 +0200

    Added title

--- wp-content/themes/topiar/templates/text-page.php
+++ wp-content/themes/topiar/templates/text-page.php
@@ -12,6 +12,7 @@ get_header();
 				<div class="text-page__date"><?php _e('Останнє оновлення:', 'tp'); ?> <?php echo get_the_modified_date('d.m.Y', get_the_ID()); ?></div>
 				<?php while( have_posts() ) { the_post(); ?>
 					<div class="text-page__content">
+						<h1 class="text-page__title"><?php the_title(); ?></h1>
 						<?php the_content(); ?>
 					</div>
 				<?php } ?>
