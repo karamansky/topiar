35bdce04cc427957e52296d6ee77549c1e1e2af8
Karamansky <karamansky@gmail.com>
2024-03-11 22:47:40 +0200

    Added partials/form

--- wp-content/themes/topiar/taxonomy-portfolio_category.php
+++ wp-content/themes/topiar/taxonomy-portfolio_category.php
@@ -115,27 +115,10 @@
 </section>
 
 
-<?php if( !empty($form['shortcode']) ) { ?>
-	<section class="archive-form" data-aos="fade-up">
-		<div class="wrapper">
-			<div class="archive-form__inner">
-				<div class="archive-form__left">
-					<?php if( !empty($form['title']) ) { ?>
-						<h2 class="archive-form__title"><?php echo $form['title']; ?></h2>
-					<?php } ?>
-					<?php if( !empty($form['description']) ) { ?>
-						<div class="archive-form__description"><?php echo $form['description']; ?></div>
-					<?php } ?>
-				</div>
-				<div class="archive-form__right">
-					<div class="archive-form__form">
-						<?php echo do_shortcode($form['shortcode']); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-<?php } ?>
+<?php if( !empty($form['shortcode']) ) {
+	echo get_template_part('template-parts/partials/form', null, ['form' => $form]);
+} ?>
+
 
 <?php
 	$args = [ 'text' => $text ];
